7baf2861d1c94e90ea1646c037c8e28649dac864
tinaOmran <145983028+tinaOmran@users.noreply.github.com>
2025-10-17 23:32:35 +0330

    feat : define function for changing status

--- task_manager.py
+++ task_manager.py
@@ -15,3 +15,20 @@ class TaskManager:
         task = Task(task_id, title, description, deadline=deadline)
         project.tasks.append(task)
         return task
+
+    def change_status(self, project_id, task_id, new_status):
+        project = next((p for p in self.storage.projects if p.id == project_id), None)
+        if not project:
+            raise ValueError("پروژه یافت نشد.")
+
+        task = next((t for t in project.tasks if t.id == task_id), None)
+        if not task:
+            raise ValueError("تسک یافت نشد.")
+
+        allowed_status = ["todo", "doing", "done"]
+        if new_status not in allowed_status:
+            raise ValueError(f"وضعیت نامعتبر است. وضعیت مجاز: {allowed_status}")
+
+        task.status = new_status
+        return task
+
