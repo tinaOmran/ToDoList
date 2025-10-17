8563a9c7696c914eae2c81e372714242b6f1ea3c
tinaOmran <145983028+tinaOmran@users.noreply.github.com>
2025-10-17 23:15:41 +0330

    feat : add limitation for tasks

--- validators.py
+++ validators.py
@@ -9,3 +9,12 @@ def validate_project(name, description, existing_projects):
         raise ValueError("پروژه‌ای با این نام از قبل وجود دارد.")
     if len(existing_projects) >= MAX_PROJECTS:
         raise ValueError("تعداد پروژه‌ها از سقف مجاز بیشتر است.")
+
+
+MAX_TASKS = int(os.getenv("MAX_NUMBER_OF_TASK", 10))
+
+def validate_task(title, description, existing_tasks):
+    if len(title.split()) > 30 or len(description.split()) > 150:
+        raise ValueError("عنوان یا توضیح تسک بیش از حد طولانی است.")
+    if len(existing_tasks) >= MAX_TASKS:
+        raise ValueError("تعداد تسک‌ها از سقف مجاز بیشتر است.")
